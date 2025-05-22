f34e45fb5513565c2b6905f6221a83312370b7ee
13asTerem <you@example.com>
2025-05-22 17:46:21 +0200

    create game class

--- VideoGame/Game.cs
+++ VideoGame/Game.cs
@@ -1,14 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace VideoGame
+public class Game
 {
-    public class Game
+    public string GameID { get; set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public int ReleaseYear { get; set; }
+    public bool IsAvailable { get; set; }
+    public decimal PricePerDay { get; set; }
+
+    // Konstruktor
+    public Game(string gameID, string title, string genre, int releaseYear, decimal pricePerDay)
     {
-     
+        GameID = gameID;
+        Title = title;
+        Genre = genre;
+        ReleaseYear = releaseYear;
+        PricePerDay = pricePerDay;
+        IsAvailable = true; // Alapértelmezett érték: kölcsönözhető
     }
 
+    // ToString metódus felülírása
+    public override string ToString()
+    {
+        return $"{Title} ({Genre}, {ReleaseYear}) - ID: {GameID}, Ár/Nap: {PricePerDay}, Elérhető: {IsAvailable}";
+    }
 }
+
