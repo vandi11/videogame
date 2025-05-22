408738e76b56408ab122e1e5a30f2fb688557760
13asTerem <you@example.com>
2025-05-22 18:02:48 +0200

    customer class

--- VideoGame/Customer.cs
+++ VideoGame/Customer.cs
@@ -8,7 +8,16 @@ namespace VideoGame
 {
     public class Customer
     {
-     
+    public int CustomerId { get; set; }
+    public string Name { get; set; }
+    public List<Game> RentedGames { get; set; }
+
+    public Customer(int customerId, string name, List<Game> rentedGames)
+        {
+            CustomerId = customerId;
+            Name = name;
+            RentedGames = rentedGames;
+        }
     }
 
 }
